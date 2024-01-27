5a10705894d13151cea69fa939e8a72a85073970
zelda <lichtseeker@gmail.com>
2024-01-26 20:12:43 -0800

    Conditionally render Start/Game screen

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from 'react'
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import Start from './screens/Start'
+import Game from "./screens/Game";
  
 const App = () => {
   const [canGameStart, setGameStart] = useState(false);
@@ -12,9 +13,16 @@ const App = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-       <StatusBar style="auto" />
-      <Start validateGameStart = {setGameStartState}/>
-      <Text>{canGameStart ? 'Game Started' : 'Game Not Started'}</Text>
+      <StatusBar style="auto" />
+      
+      {canGameStart ? (
+        <Game />
+      ) : (
+        <Start validateGameStart={setGameStartState} />
+      )}
+
+      
+      <Text>{canGameStart ? 'Game Started!!' : 'Game Not Started'}</Text>
     </SafeAreaView>
   )
 }
